1574a9f50f639f5a51e19f852b1f02addec9b833
angryduck <72322679+nogoduck@users.noreply.github.com>
2021-11-20 02:28:28 +0900

    change key

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ad:1114@table0.zotlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-};
-// "mongodb+srv://root:root@cluster0.aamgd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
